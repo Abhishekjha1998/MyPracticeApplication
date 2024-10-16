7816567440a05fc99eea17aa54e0727556016aa5
Abhishekjha1998 <abishekjha110524@gmail.com>
2024-10-16 09:19:54 +0530

    my first changes.

--- src/main/java/MyTest.java
+++ src/main/java/MyTest.java
@@ -1,4 +1,9 @@
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class MyTest {
+    @GetMapping("test")
     private void test(){
         System.out.println("This is my test class");
     }
